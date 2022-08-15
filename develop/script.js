15b368fa5740406f0da283ad545c0ca551a96e48
Indigo <indigo.crandell@gmail.com>
2022-08-15 15:41:49 -0400

    Yay good stuff. The click works! Now to clear the search history.

--- develop/script.js
+++ develop/script.js
@@ -39,7 +39,7 @@ for (i = 0; i < data.length; i++) {
     storedCity.addEventListener('click', function(event) {
         var element = event.target;
         var index = element.getAttribute('data-index');
-    console.log(data[index]);
+    searchApi(data[index]);
         
         
 
